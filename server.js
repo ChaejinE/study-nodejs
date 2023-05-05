60599cb32e0943d8aedb35fb416d1eb8b72712a8
cjlotto <joung6517@gmail.com>
2023-05-05 23:06:41 +0900

    Bootsrap UI

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.listen(8080, function () { console.log('listening on 8080') });
-app.get('/pet', function (요청, 응답) { 응답.send("Pet Site") });
 app.get('/beauty', (request, response) => { response.send("Beuaty Page") });
+app.get('/', function (request, response) { response.sendFile(__dirname + "/index.html") });
+app.get('/write', (request, response) => { response.sendFile(__dirname + "/write.html") });
