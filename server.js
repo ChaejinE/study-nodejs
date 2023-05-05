13d2972fa374761e25e4f2a6f0872345e30fe216
cjlotto <joung6517@gmail.com>
2023-05-05 23:34:30 +0900

    POST using form

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () { console.log('listening on 8080') });
 app.get('/beauty', (request, response) => { response.send("Beuaty Page") });
 app.get('/', function (request, response) { response.sendFile(__dirname + "/index.html") });
 app.get('/write', (request, response) => { response.sendFile(__dirname + "/write.html") });
+
+app.post('/add', (request, response) => { console.log(request.body.title) });
