c03938c81ae1cfcde70e79323c927245a297fb8b
cjlotto <joung6517@gmail.com>
2023-05-06 00:39:46 +0900

    DB내 데이터 list 보여주기

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs')
 
 var db;
 MongoClient.connect("mongodb+srv://cjlotto:1234@cluster0.x4bssi5.mongodb.net/?retryWrites=true&w=majority",
@@ -9,14 +10,20 @@ MongoClient.connect("mongodb+srv://cjlotto:1234@cluster0.x4bssi5.mongodb.net/?re
         if (err) return console.log(err);
 
         db = client.db("TODOApp");
-        var data = { 이름: "John", 나이: 19 };
-        db.collection('post').insertOne(data, (err, result) => { console.log("good") });
         app.listen(8080, function () { console.log('listening on 8080') });
     })
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/beauty', (request, response) => { response.send("Beuaty Page") });
 app.get('/', function (request, response) { response.sendFile(__dirname + "/index.html") });
 app.get('/write', (request, response) => { response.sendFile(__dirname + "/write.html") });
+app.get('/list', (request, response) => {
+    db.collection('post').find().toArray((err, result) => {
+        console.log(result);
+        response.render('list.ejs', { posts: result });
+    })
+});
 
-app.post('/add', (request, response) => { console.log(request.body.title) });
+app.post('/add', (request, response) => {
+    var data = { 제목: request.body.title, 날짜: request.body.date };
+    db.collection('post').insertOne(data, (err, result) => { console.log("good") });
+});
